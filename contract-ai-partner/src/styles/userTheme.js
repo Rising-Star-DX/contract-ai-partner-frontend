e252f647b8a41d6d315a407d665dde9f6f6226ec
wizdom972 <jihae0584@gmail.com>
2025-03-05 13:29:38 +0900

    💄 나눔스퀘어 네오 폰트 추가가

---
+++ contract-ai-partner/src/styles/userTheme.js
@@ -0,0 +1,21 @@
+import { createTheme} from '@mui/material/styles';
+
+const userTheme = createTheme({
+    palette: {
+        primary:{
+            main: "#5686E1"
+        }
+    },
+    typography: {
+        fontFamily: [
+            'NanumSquareNeo',
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif'
+        ].join(",")
+    }
+})
