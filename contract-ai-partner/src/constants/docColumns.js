1434b5005c87be926c7adf020b48c99bf2008e4b
wizdom972 <jihae0584@gmail.com>
2025-03-24 15:25:21 +0900

    💄 문서 상태 AI 관련 상태만 존재하도록 변경

--- contract-ai-partner/src/constants/docColumns.js
+++ contract-ai-partner/src/constants/docColumns.js
@@ -2,31 +2,20 @@
 import React from "react";
 import { Box, Button, IconButton, Chip } from "@mui/material";
 import RemoveCircleOutlineOutlinedIcon from "@mui/icons-material/RemoveCircleOutlineOutlined";
-import CloudUploadIcon from "@mui/icons-material/CloudUpload"; // 업로드 관련 아이콘
 import PsychologyIcon from "@mui/icons-material/Psychology"; // AI 분석 관련 아이콘
 import { getDocIcon } from "../utils/docUtils";
 
 // 상태에 따른 Chip을 반환하는 함수
 const getStatusChip = (status) => {
-    let icon = null;
+    const icon = <PsychologyIcon fontSize="small" />;
     let color = "default";
 
-    if (status.includes("업로드")) {
-        icon = <CloudUploadIcon fontSize="small" />;
-        if (status.includes("실패")) {
-            color = "error";
-        } else {
-            color = "success";
-        }
-    } else if (status.includes("AI 분석")) {
-        icon = <PsychologyIcon fontSize="small" />;
-        if (status.includes("완료")) {
-            color = "secondary";
-        } else if (status.includes("실패")) {
-            color = "error";
-        } else {
-            color = "primary";
-        }
+    if (status.includes("완료")) {
+        color = "success";
+    } else if (status.includes("실패")) {
+        color = "error";
+    } else {
+        color = "warning";
     }
 
     return <Chip icon={icon} label={status} color={color} sx={{ pl: 1 }} />;
@@ -47,14 +36,14 @@ const DOC_COLUMNS = (onRowView, onRowDelete) => [
                 />
                 <Box sx={{ ml: 4 }}>{params.value}</Box>
             </Box>
-        ),
+        )
     },
     {
         field: "category",
         headerName: "카테고리",
         flex: 1,
         headerAlign: "center",
-        align: "center",
+        align: "center"
     },
     {
         field: "uploadDate",
@@ -63,23 +52,17 @@ const DOC_COLUMNS = (onRowView, onRowDelete) => [
         type: "dateTime",
         headerAlign: "center",
         align: "center",
-        valueGetter: (params) => new Date(params),
+        valueGetter: (params) => new Date(params)
     },
     {
         field: "status",
         headerName: "문서 상태",
         flex: 0.7,
         type: "singleSelect",
-        valueOptions: [
-            "업로드 중",
-            "업로드 실패",
-            "AI 분석 중",
-            "AI 분석 완료",
-            "AI 분석 실패",
-        ],
+        valueOptions: ["AI 분석 중", "AI 분석 완료", "AI 분석 실패"],
         headerAlign: "center",
         align: "center",
-        renderCell: (params) => getStatusChip(params.value),
+        renderCell: (params) => getStatusChip(params.value)
     },
     {
         field: "view",
@@ -96,7 +79,7 @@ const DOC_COLUMNS = (onRowView, onRowDelete) => [
             >
                 문서 보기
             </Button>
-        ),
+        )
     },
     {
         field: "delete",
@@ -112,8 +95,8 @@ const DOC_COLUMNS = (onRowView, onRowDelete) => [
             >
                 <RemoveCircleOutlineOutlinedIcon />
             </IconButton>
-        ),
-    },
+        )
+    }
 ];
 
 export default DOC_COLUMNS;
